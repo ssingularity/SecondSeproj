fc82569377cc63b39eebd4b99232fa7f3958a95a
ssingularity <1755405704@qq.com>
2017-12-18 22:39:10 +0800

    add side navigation and css

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   	get '/help', to: 'static_pages#help' 
  	get '/contact', to: 'static_pages#contact'
   	get '/about', to: 'static_pages#about'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
