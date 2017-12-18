52bce49d91acc5e697360e13bc03db8a633f0748
ssingularity <1755405704@qq.com>
2017-12-19 01:32:36 +0800

    add routes classes and styles

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,19 @@ Rails.application.routes.draw do
   	get '/help', to: 'static_pages#help' 
  	get '/contact', to: 'static_pages#contact'
   	get '/about', to: 'static_pages#about'
+  	get '/backup', to: 'static_pages#backup'
+# Add class
+  	get '/diskregu', to: 'static_pages#diskregu'
+  	get '/diskmain', to: 'static_pages#diskmain'
+  	get '/devregu', to: 'static_pages#devregu'
+  	get '/internet', to: 'static_pages#internet'
+  	get '/edit', to: 'static_pages#edit'
+  	get '/trans', to: 'static_pages#trans'
+  	get '/fileregu', to: 'static_pages#fileregu'
+  	get '/sysregu', to: 'static_pages#sysregu'
+  	get '/sysconf', to: 'static_pages#sysconf'
+# Add elements
+  	get '/compress', to: 'static_pages#compress'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
