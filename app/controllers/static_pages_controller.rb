97da362363eb5300b7d1d92ab24986e9d525bf13
ssingularity <1755405704@qq.com>
2017-12-19 12:56:04 +0800

    add the class title

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,26 @@
 class StaticPagesController < ApplicationController
   def home
+  	@elements={
+  		compress: compress_path,
+  		bzip2: bzip2_path,
+  		bunzip2: bunzip2_path,
+  		ar: ar_path,
+  		bzip2recover: bzip2recover_path,
+  		unarj: unarj_path,
+  		tar: tar_path,
+  		gzexe: gzexe_path,
+  		uudecode: uudecode_path,
+  		unzip: unzip_path,
+  		uuencode: uuencode_path,
+  		zipinfo: zipinfo_path,
+  		zip: zip_path,
+  		restore: restore_path,
+  		dump: dump_path,
+  		cpio: cpio_path,
+  		gunzip: gunzip_path,
+  		lha: lha_path,
+  		gzip: gzip_path
+  	}
   end
 
   def help
